579a540cb7f850c6e901dd2010263c7f30fc9e7f
Fabrizio Turco <fturco@gmx.de>
2021-06-24 14:33:45 +0200

    Skripte für Frequenzbereich hinzugefuegt

---
+++ 1_Frequenzbereich/FourierTrafo.m
@@ -0,0 +1,44 @@
+%% 
+t_start = 1000;
+t_end = 1100;
+
+%%
+[X, U, t_vec] = createStateAndInput(t_start, t_end);
+
+% ACHTUNG, NOCH BESSER LÖSEN!
+U(1,:)=U(2,:);
+%
+
+tdiff = zeros(length(t_vec)-1,1);
+for i=1:length(t_vec)-1
+    tdiff(i)=t_vec(i+1,1)-t_vec(i,1);
+end
+
+Fs = 1/mean(tdiff);            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = length(t_vec);             % Length of signal
+f = Fs*(0:(L/2))/L;
+
+% Fourier-Trafo Zustand
+Y=fft(X);
+P2 = Y/L;
+P1 = P2(1:L/2+1,:);
+P1(2:end-1,:) = 2*P1(2:end-1,:);
+X_Fourier = Y(1:L/2+1,:);
+
+% Fourier-Trafo Steuergrößen
+Y=fft(U);
+P2_U = Y/L;
+P1_U = P2_U(1:L/2+1,:);
+P1_U(2:end-1,:) = 2*P1_U(2:end-1,:);
+U_Fourier = Y(1:L/2+1,:);
+
+%% Berechnung der Übertragungsmatrix für jede Frequenz
+G = zeros(4,2,L/2);
+for k=1:L/2
+    for i=1:4
+        for j=1:2
+            G(i,j,k) = X_Fourier(k,i)/U_Fourier(k,j);
+        end
+    end
+end
