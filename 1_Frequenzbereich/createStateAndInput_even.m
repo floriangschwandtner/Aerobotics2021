9fc80bbc4dd3ca7c4a6b1dd2ae08233d48b21a52
Fabrizio Turco <fturco@gmx.de>
2021-07-16 16:48:32 +0200

    Daten für Seitenbewegung hinzugefuegt

--- 1_Frequenzbereich/createStateAndInput_even.m
+++ 1_Frequenzbereich/createStateAndInput_even.m
@@ -4,24 +4,25 @@
 %2021-06-24 FT: Interpolation für alle Variablen hinzugefügt, sodass
 %Abtastrate gleichmäßig
 
-
-load("data.mat")
+clear
+load("x_state_original.mat")
+load("u_control_original.mat")
 
 % Bestimmung der mittleren Abtastrate
-tdiff = zeros(length(t)-1,1);
-for i=1:length(t)-1
+tdiff = zeros(length(x_1(:,1))-1,1);
+for i=1:length(x_1(:,1))-1
     tdiff(i)=t(i+1)-t(i);
 end
 dt = mean(tdiff);
 
 t_vec = t(1):dt:t(end);
 
-x_1Inter = interp1(t, x(:,1),(t_vec))';
-x_2Inter = interp1(t, x(:,2),(t_vec))';
-x_3Inter = interp1(t, x(:,3),(t_vec))';
-x_4Inter = interp1(t, x(:,4),(t_vec))';
-u_1Inter = interp1(t, u(:,1),(t_vec))';
-u_2Inter = interp1(t, u(:,2),(t_vec))';
+x_1Inter = interp1(x_1(:,1), x_1(:,2),(t_vec));
+x_2Inter = interp1(x_2(:,1), x_2(:,2),(t_vec));
+x_3Inter = interp1(x_3(:,1), x_3(:,2),(t_vec));
+x_4Inter = interp1(x_4(:,1), x_4(:,2),(t_vec));
+u_1Inter = interp1(u_1(:,1), u_1(:,2),(t_vec));
+u_2Inter = interp1(u_2(:,1), u_2(:,2),(t_vec));
 
 X = [x_1Inter, x_2Inter, x_3Inter, x_4Inter];
 U = [u_1Inter, u_2Inter];
