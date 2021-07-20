2f5dd2b372d8b47371de1167b1e2d2f072115087
Florian Gschwandtner <st149835@stud.uni-stuttgart.de>
2021-07-20 12:28:15 +0200

    Umbau diskret und kontinuierlich umschaltbar

--- Simulator/ZRD_laengsbewegung.m
+++ Simulator/ZRD_laengsbewegung.m
@@ -4,6 +4,10 @@ clc
 clear all
 close all
 
+%% Switch kontinuierlich/diskret
+
+simKontinuierlich = true;
+
 %% Daten-Vorbereitung %%%%%
 load("data_laengs_even.mat");
 
@@ -13,7 +17,7 @@ n2 = 78419;  %Endindex
 dFilt =0.3; 
 fEck  =20;    %Hz
 dt = 0.0267;  % Zeitschritt
-g    = 9.81;
+    g    = 9.81;
 
 % Anfangswerte
 a0   = 0.0167;   
@@ -23,15 +27,6 @@ gamma0 = 0.0027  ;
 q0    = 0;
 nu0   = -0.1698;
 df0   = 0.4271; 
-% Interpolation 
-
-% x1=interp1(t(n1:n2),x(n1:n2,1)-a0,(t(n1):dt:t(n2))');
-% x2=interp1(t(n1:n2),x(n1:n2,2),(t(n1):dt:t(n2))');
-% x3=interp1(t(n1:n2),x(n1:n2,3)-V0,(t(n1):dt:t(n2))');
-% x4=interp1(t(n1:n2),x(n1:n2,4)-gamma0,(t(n1):dt:t(n2))');
-% u1=interp1(t(n1:n2),u(n1:n2,1)+ 0.1326,(t(n1):dt:t(n2))');
-% u2=interp1(t(n1:n2),u(n1:n2,2)-0.4244,(t(n1):dt:t(n2))');
-% time = (t(n1):dt:t(n2))' ;
 
 time = t(n1:n2);
 X    = x(n1:n2,:);
@@ -44,8 +39,6 @@ u1   = U(:,1)-nu0;%- U(1,1);
 u2   = U(:,2)-df0;%- U(1,2); 
 
 %% Filterung %%%%%%
-
-
 [x1filt, x1_dot] = filteredDerivative1(x1, dt, fEck,dFilt);
 [x2filt, x2_dot] = filteredDerivative1(x2, dt, fEck,dFilt);
 [x3filt, x3_dot] = filteredDerivative1(x3, dt, fEck,dFilt);
@@ -56,10 +49,6 @@ u2   = U(:,2)-df0;%- U(1,2);
 
 Xfilt=[x1filt,x2filt,x3filt,x4filt];
 Ufilt=[u1filt,u2filt];
-
-
-
-
 
 %% LSQ- Schätzer 
 % Beiwerte 
@@ -81,19 +70,44 @@ Mdf  = xhat(12);
 Xeta = xhat(3);
 Zeta = xhat(7);
 
-
 %% Matrix-Schätzer
-z = [x1filt(2:length(x1filt)), x2filt(2:length(x1filt)), x3filt(2:length(x1filt)), x4filt(2:length(x1filt))];
-%z = [x1_dot, x2_dot, x3_dot, x4_dot];
-H = [x1filt(1:length(x1filt)-1), x2filt(1:length(x1filt)-1), x3filt(1:length(x1filt)-1), x4filt(1:length(x1filt)-1), u1filt(1:length(x1filt)-1), u2filt(1:length(x1filt)-1)];
-%H = [x1filt, x2filt, x3filt, x4filt, u1filt, u2filt];
 
-xhat1 = (H'*H)\H'*z;
+if(simKontinuierlich)
+    % Daten für kontinuierliches ZR Modell:
+    z = [x1_dot, x2_dot, x3_dot, x4_dot];
+    % H Matrix für Schätzer kontinuierlich
+    H = [x1filt, x2filt, x3filt, x4filt, u1filt, u2filt];
+    
+    % Lineare Schätzgleichung
+    xhat1 = (H'*H)\H'*z;
+    
+    % Zusammenbau der Matrizen kontinuierlich:
+    A1 = xhat1(1:4, 1:4)';
+    B1 = xhat1(5:6, 1:4)';
+else
+    % Daten für diskretes ZR Modell:
+    z = [x1filt(2:length(x1filt)), x2filt(2:length(x1filt)), x3filt(2:length(x1filt)), x4filt(2:length(x1filt))];
+    % H Matrix für Schätzer diskret
+    H = [x1filt(1:length(x1filt)-1), x2filt(1:length(x1filt)-1), x3filt(1:length(x1filt)-1), x4filt(1:length(x1filt)-1), u1filt(1:length(x1filt)-1), u2filt(1:length(x1filt)-1)];
+    
+    % Lineare Schätzgleichung
+    xhat1 = (H'*H)\H'*z;
 
+    % Zusammenbau der Matrizen diskret:
+    A1 = (xhat1(1:4, 1:4)'-eye(4,4))/dt
+    B1 = xhat1(5:6, 1:4)'/dt
+end
+
+
+
+
+
+
+% Zusammenbau der Matritzen LSQ:
 A  = [Za/V0, 1, Zv/V0, 0; Ma, Mq, Mv, 0; Xa, 0, Xv, -g; -Za/V0, 0, -Zv/V0, 0]
-A1 = (xhat1(1:4, 1:4)'-eye(4,4))/dt
 B  = [Zeta/V0, -Xdf/V0*sin(a0+i_f); Meta, Mdf; Xeta, Xdf*cos(a0+i_f); -Zeta/V0, Xdf/V0*sin(a0+i_f)]
-B1 = xhat1(5:6, 1:4)'/dt
+
+
 
 
 %% Simulation %%%%%%
@@ -105,6 +119,7 @@ output = sim('ZRD_laengs_Simulator.slx', time);
 
 %% LSQ Schätzer %%%%%%
 figure(1)
+sgtitle("LSQ Schätzer")
 subplot(2,2,1)
 
 plot(output.tout, output.ysim(:,1)+a0)
@@ -153,6 +168,7 @@ legend('sim','filt')
 %% A&B Schätzer %%%%%%%%
 
 figure(2)
+sgtitle("Simulation und gefilterte Messdaten")
 subplot(2,2,1)
 
 plot(output.tout, output.ysim1(:,1)+a0)
@@ -216,6 +232,7 @@ legend('sim','filt')
 % ylabel('dftrue')
 
 figure(3)
+sgtitle("Vergleich gefilterte und ungefilterte Daten")
 subplot(2,2,1)
 
 % plot(time(1:length(time)-1),x1_dot)
